84449b4c3f6c8a02cbbedf2e3b25367410548b5c
Solomon Hufford <solo.hufford@gmail.com>
2023-08-31 09:09:46 -0500

    Updated security, and emails.
    Updated security with fresh token generation, along with also updated reset password, and emails. Also fixed emails format so that emails will send properly.

--- config.py
+++ config.py
@@ -4,12 +4,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
                    ['true', 'on', '1']
-    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
