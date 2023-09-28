93d4526c17d0e31a403d810dee4c910819cab9dc
Solomon Hufford <solo.hufford@gmail.com>
2023-09-27 22:48:06 -0500

    Created comments and touched up post
    Worked on making post better and comments. I also began to work on the moderation possibilities for comments.

--- config.py
+++ config.py
@@ -17,6 +17,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_POSTS_PER_PAGE = int(os.environ.get('FLASKY_POSTS_PER_PAGE'))
     FLASKY_FOLLOWERS_PER_PAGE = int(os.environ.get('FLASKY_FOLLOWERS_PER_PAGE'))
+    FLASKY_COMMENTS_PER_PAGE = 8
 
     @staticmethod
     def init_app(app):
